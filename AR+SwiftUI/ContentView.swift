b55c3d8f0ca89c4f3a890de314cc0fc24a836b20
mgipson <mmgipson3@gmail.com>
2020-06-12 22:17:09 -0400

    Initial Commit

---
+++ AR+SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AR+SwiftUI
+//
+//  Created by Madison Gipson on 6/12/20.
+//  Copyright © 2020 Madison Gipson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
