115079338dd34e1035259a51bda649dea0918ef0
Madison Gipson <46686097+mgipson@users.noreply.github.com>
2020-09-25 14:55:23 -0400

    Add Code to ARView- didn't get uploaded initially.

--- AR+SwiftUI/ARView.swift
+++ AR+SwiftUI/ARView.swift
@@ -1,9 +1,56 @@
-//
-//  ARView.swift
-//  AR+SwiftUI
-//
-//  Created by Madison Gipson on 6/12/20.
-//  Copyright © 2020 Madison Gipson. All rights reserved.
-//
-
 import Foundation
+import ARKit
+import SwiftUI
+
+// MARK: - ARViewIndicator
+struct ARViewIndicator: UIViewControllerRepresentable {
+   typealias UIViewControllerType = ARView
+   
+   func makeUIViewController(context: Context) -> ARView {
+      return ARView()
+   }
+   func updateUIViewController(_ uiViewController:
+   ARViewIndicator.UIViewControllerType, context:
+   UIViewControllerRepresentableContext<ARViewIndicator>) { }
+}
+
+
+class ARView: UIViewController, ARSCNViewDelegate {
+   
+   var arView: ARSCNView {
+      return self.view as! ARSCNView
+   }
+   override func loadView() {
+     self.view = ARSCNView(frame: .zero)
+   }
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      arView.delegate = self
+      arView.scene = SCNScene()
+   }
+   // MARK: - Functions for standard AR view handling
+   override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+   }
+   override func viewDidLayoutSubviews() {
+      super.viewDidLayoutSubviews()
+   }
+   override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      let configuration = ARWorldTrackingConfiguration()
+      arView.session.run(configuration)
+      arView.delegate = self
+   }
+   override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      arView.session.pause()
+   }
+   // MARK: - ARSCNViewDelegate
+   func sessionWasInterrupted(_ session: ARSession) {}
+   
+   func sessionInterruptionEnded(_ session: ARSession) {}
+   func session(_ session: ARSession, didFailWithError error: Error)
+   {}
+   func session(_ session: ARSession, cameraDidChangeTrackingState
+   camera: ARCamera) {}
+}
