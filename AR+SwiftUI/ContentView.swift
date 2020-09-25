d574e5e90948e48d01bd36480ce760ecfb592926
Madison Gipson <46686097+mgipson@users.noreply.github.com>
2020-09-25 14:56:27 -0400

    Upload ContentView- didn't get uploaded initially.

--- AR+SwiftUI/ContentView.swift
+++ AR+SwiftUI/ContentView.swift
@@ -8,10 +8,42 @@
 
 import SwiftUI
 
+// MARK: - NavigationIndicator
+struct NavigationIndicator: UIViewControllerRepresentable {
+   typealias UIViewControllerType = ARView
+   func makeUIViewController(context: Context) -> ARView {
+      return ARView()
+   }
+   func updateUIViewController(_ uiViewController:
+   NavigationIndicator.UIViewControllerType, context:
+   UIViewControllerRepresentableContext<NavigationIndicator>) { }
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+   @State var page = "Home"
+   
+   var body: some View {
+      VStack {
+         if page == "Home" {
+            Button("Switch to ARView") {
+               self.page = "ARView"
+            }
+         } else if page == "ARView" {
+            ZStack {
+               NavigationIndicator()
+               VStack {
+                  Spacer()
+                  Spacer()
+                  Button("Home") {
+                     self.page = "Home"
+                  }.padding()
+                  .background(RoundedRectangle(cornerRadius: 10)
+                  .foregroundColor(Color.white).opacity(0.7))
+               }
+            }
+         }
+      }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
